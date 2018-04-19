ee252199306f54388b1506381cc9d89a17a98fca
Mike Wilson <knobby@burntsheep.com>
2018-04-19 12:01:14 -0400

    Updating provides to work with nfs charm. Changed the interface based on cory_fu's suggestions.

--- provides.py
+++ provides.py
@@ -1,23 +1,33 @@
-from charmhelpers.core import hookenv
-from charms.reactive import when, when_not
+from charms.reactive import when_any
 from charms.reactive import set_flag, clear_flag
 from charms.reactive import Endpoint
 
 
 class MountProvides(Endpoint):
 
-    @when('endpoint.{endpoint_name}.joined')
-    def joined(self):
-        set_flag(self.expand_name('{endpoint_name}.available'))
+    @when_any('endpoint.{endpoint_name}.changed',
+              'endpoint.{endpoint_name}.departed')
+    def changed(self):
+        set_flag(self.expand_name('{endpoint_name}.changed'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.departed'))
 
-    @when_not('endpoint.{endpoint_name}.joined')
-    def broken(self):
-        clear_flag(self.expand_name('{endpoint_name}.available'))
+    def get_mount_requests(self):
+        return [{
+            'identifier': relation.relation_id,
+            'application_name': relation.application_name,
+            'addresses': [
+                unit.received_raw.get('ingress-address',
+                                      unit.received_raw['private-address'])
+                for unit in relation.joined_units],
+        } for relation in self.relations]
 
-    def configure(self, mountpoint, fstype, options):
-        for relation in self.relations:
+    def configure(self, responses):
+        for response in responses:
+            relation = self.relations[response['identifier']]
             relation.to_publish_raw.update({
-                'mountpoint': mountpoint,
-                'fstype': fstype,
-                'options': options
+                'mountpoint': response['mountpoint'],
+                'fstype': response['fstype'],
+                'options': response['options'],
             })
+        clear_flag(self.expand_name('{endpoint_name}.changed'))
